163de2c14c5aed9067434702b9d6fce8be870260
Ari Porad <ari@porad.com>
2015-07-09 12:32:32 -0700

    Removed some dummy testing methods

--- scripts/test1.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(robot) {
-	robot.respond(/test2/i, function(msg){
-		msg.reply('Winner!!');
-	})
-	robot.respond(/king/i, function(msg){
-		msg.reply('All Hail The King, Ari Porad!');
-	})
-}
