e47e91f75f256c2e65ab4b734ac87d06f0e0165b
Ari Porad <ari@porad.com>
2015-07-11 15:00:23 -0700

    Added logging to keepalive.

--- scripts/keepawake.js
+++ scripts/keepawake.js
@@ -1,5 +1,8 @@
 module.exports = function (robot) {
 	setInterval(function(){
-		robot.adapter.client.setPresence("active", function(){});
+		robot.adapter.client.setPresence("active", function (res) {
+			console.log("keepalive res:");
+			console.log(res);
+		});
 	}, 14 * 60 * 1000);
 }
