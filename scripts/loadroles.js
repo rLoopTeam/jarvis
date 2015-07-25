803c0d323202e497d2d35ee18f6845392cfdf2ec
Ari Porad <ari@porad.com>
2015-07-25 13:59:25 -0700

    Disabled role loading

--- scripts/loadroles.js
+++ scripts/loadroles.js
@@ -1,15 +1,15 @@
-var data = require('./roles.json');
+// var data = require('./roles.json');
 
-module.exports = function(robot) {
-	setTimeout(function(){
-		for (var i = 0; i < data.length; i++) {
-			var user = robot.brain.userForName(data[i].name);
-			console.log(data[i]);
-			console.log(user);
-			console.log('\n\n\n');
-			if (!user) continue;
-			user.roles = (user.roles || []).concat(data[i].roles.split(','), ['rolesLoaded']);
-		}
-	}, 10000);
+// module.exports = function(robot) {
+// 	setTimeout(function(){
+// 		for (var i = 0; i < data.length; i++) {
+// 			var user = robot.brain.userForName(data[i].name);
+// 			console.log(data[i]);
+// 			console.log(user);
+// 			console.log('\n\n\n');
+// 			if (!user) continue;
+// 			user.roles = (user.roles || []).concat(data[i].roles.split(','), ['rolesLoaded']);
+// 		}
+// 	}, 10000);
 	
-}
+// }
