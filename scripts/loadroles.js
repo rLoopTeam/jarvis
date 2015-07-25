e97aa48897edd5ac5a82e4cdbd2ee6c7416464c7
Ari Porad <ari@porad.com>
2015-07-25 13:47:56 -0700

    Fixed a crash

--- scripts/loadroles.js
+++ scripts/loadroles.js
@@ -3,6 +3,9 @@ var data = require('./roles.json');
 module.exports = function(robot) {
 	for (var i = 0; i < data.length; i++) {
 		var user = robot.brain.userForName(data[i].name);
+		console.log(data[i]);
+		console.log(user);
+		console.log('\n\n\n');
 		user.roles = user.roles.concat(data[i].split(','));
 	}
 }
